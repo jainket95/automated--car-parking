d3722d5222fb50c3aea4a0fa196d07fbcf310d76
jainket95 <aniket1995jain@gmail.com>
2020-08-18 01:01:39 +0530

    initial value

---
+++ src/App.js
@@ -0,0 +1,180 @@
+import React, { useState, useEffect } from 'react';
+import carJson from './car';
+import './App.css';
+
+const App = () => {
+  const [slots, setSlots] = useState(10);
+  const [carParked, setCarParked] = useState(5);
+  const [carNumber, setCarNumber] = useState('');
+  const [color, setColor] = useState('');
+  const [cars, setCars] = useState([]);
+  const [searchCar, setSearchCar] = useState(null);
+
+  useEffect(() => {
+    setCars(carJson);
+    //eslint-disable-next-line
+  }, []);
+
+  const onSubmit = () => {
+    console.log(slots, carParked, cars);
+  };
+
+  const onSearch = () => {
+    const newLook = cars.filter(
+      car => car.car_number === carNumber || car.color === color
+    );
+    setCars(newLook);
+  };
+
+  const onClear = () => {
+    if (carNumber !== '' || color !== '') {
+      setCarNumber('');
+      setColor('');
+    }
+  };
+
+  const onReset = () => {
+    console.log('clicked');
+    onClear();
+    setCars(carJson);
+  };
+
+  const onAdd = () => {
+    var newSlot = cars.reduce((acc, curr) => {
+      if (acc < curr.slot_number) acc = curr.slot_number;
+      return acc;
+    }, 0);
+
+    var newData = {
+      id: newSlot + 1,
+      car_number: carNumber,
+      color: color,
+      slot_number: newSlot + 1,
+      date: new Date().toLocaleString(),
+    };
+
+    if (carNumber !== '' || color !== '') {
+      var addOn = [...cars, newData];
+      setCars(addOn);
+      var newSlot = carParked + 1;
+      setCarParked(newSlot);
+    }
+    onClear();
+  };
+
+  const onDelete = i => {
+    console.log(i);
+  };
+
+  if (slots === '' || carParked === '') {
+    return (
+      <div className="center complete_view">
+        <h4>Enter the Parking Details.</h4>
+        <form className="center">
+          <div className="input-form">
+            <label htmlFor="slots">No. of parking slots: </label>
+            <input
+              type="text"
+              name="slots"
+              value={slots}
+              onChange={e => setSlots(+e.target.value)}
+            />
+          </div>
+
+          <div className="input-form">
+            <label htmlFor="slots">No. of car parked: </label>
+            <input
+              type="text"
+              name="slots"
+              value={carParked}
+              onChange={e => setCarParked(+e.target.value)}
+            />
+          </div>
+
+          <button className="button" href="#!" type="button" onClick={onSubmit}>
+            Enter
+          </button>
+        </form>
+      </div>
+    );
+  }
+
+  return (
+    <div className="main_container">
+      <nav>
+        <header>Automated Parking Tool</header>
+      </nav>
+
+      <div>
+        <h4>Current Status</h4>
+        <p>
+          Number of parking places: <span>{slots}</span>
+        </p>
+        <p>
+          Number of parked car: <span>{carParked}</span>
+        </p>
+      </div>
+
+      <div className="search">
+        <form>
+          <input
+            type="text"
+            name="car_number"
+            value={carNumber}
+            onChange={e => setCarNumber(e.target.value)}
+            placeholder="Type Reg No."
+          />
+          <input
+            type="text"
+            name="color"
+            value={color}
+            onChange={e => setColor(e.target.value)}
+            placeholder="Enter Color"
+          />
+          <button className="button" type="button" onClick={onSearch}>
+            Search
+          </button>
+          <button className="button" type="button" onClick={onReset}>
+            Reset
+          </button>
+          <button className="button" type="button" onClick={onAdd}>
+            Add
+          </button>
+        </form>
+      </div>
+
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Car No.</th>
+              <th>Color</th>
+              <th>Slot No.</th>
+              <th>Date Time</th>
+              <th>Delete the entry</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cars.map((carEntry, i) => (
+              <tr key={carEntry.id}>
+                <td>{carEntry.id}</td>
+                <td>{carEntry.car_number}</td>
+                <td>{carEntry.color}</td>
+                <td>{carEntry.slot_number}</td>
+                <td>{carEntry.date}</td>
+                <td>
+                  <a href="#!" className="button" onClick={onDelete(i)}>
+                    Remove
+                  </a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/car.js
@@ -0,0 +1,39 @@
+const carJson = [
+  {
+    id: 1,
+    car_number: 'KA-64-YX-0619',
+    color: 'red',
+    slot_number: 3,
+    date: new Date().toLocaleString(),
+  },
+  {
+    id: 2,
+    car_number: 'KA-23-KJ-3234',
+    color: 'orange',
+    slot_number: 4,
+    date: new Date().toLocaleString(),
+  },
+  {
+    id: 3,
+    car_number: 'KA-37-YX-5824',
+    color: 'black',
+    slot_number: 5,
+    date: new Date().toLocaleString(),
+  },
+  {
+    id: 4,
+    car_number: 'KA-64-AK-8192',
+    color: 'blue',
+    slot_number: 1,
+    date: new Date().toLocaleString(),
+  },
+  {
+    id: 5,
+    car_number: 'KA-64-YX-0233',
+    color: 'blue',
+    slot_number: 2,
+    date: new Date().toLocaleString(),
+  },
+];
+
+export default carJson;
